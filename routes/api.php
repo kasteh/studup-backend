2bcedcaa551cc0f8aacc5e525629d97f42784f7f
Rakotoniaina Rajo Mampionona <rajomampiononarakotoniaina@gmail.com>
2025-09-09 17:53:04 +0300

    UPDATE: install API

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Routes protégées par Sanctum
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
