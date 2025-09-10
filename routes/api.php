43e026a1f40ac58199ae95c2381976b9e0f7c71f
Rakotoniaina Rajo Mampionona <rajomampiononarakotoniaina@gmail.com>
2025-09-11 00:45:53 +0300

    ADD: routes login

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,9 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register'])->name('users.register')->middleware('throttle:100,1');
     Route::post('/send-verification-code', [VerificationController::class, 'sendVerificationCode']);
     Route::post('/verify-code', [VerificationController::class, 'verifyCode']);
+    Route::post('/login', [AuthController::class, 'login'])->name('users.login');
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/login', [AuthController::class, 'login'])->name('users.login');
+        //
     });
 });
